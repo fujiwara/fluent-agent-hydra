589b7316fcf5f999101345ecad40b8ad6bafbcee
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-08-06 16:08:48 +0900

    tailing filename is converted from rel to abs

--- hydra/trail.go
+++ hydra/trail.go
@@ -4,11 +4,23 @@ import (
 	"github.com/howeyc/fsnotify"
 	"io"
 	"log"
+	"os"
 	"path/filepath"
 )
 
 // Trail follow the tail of file and post BulkMessage to channel.
 func Trail(filename string, tag string, ch chan *BulkMessage) {
+	defer log.Println("[error] Aborted to trail", filename)
+
+	if !filepath.IsAbs(filename) { // rel path to abs path
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Println("[error] Couldn't get current working dir.", err)
+			return
+		}
+		filename = filepath.Join(cwd, filename)
+	}
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Println("[error] Couldn't create file watcher", err)
@@ -17,13 +29,13 @@ func Trail(filename string, tag string, ch chan *BulkMessage) {
 	defer watcher.Close()
 
 	parent := filepath.Dir(filename)
-	log.Println("[info] watching events for directory", parent)
+	log.Println("[info] watching events of directory", parent)
 	err = watcher.Watch(parent)
 	if err != nil {
-		log.Println("[error] Couldn't watch event for", parent, err)
+		log.Println("[error] Couldn't watch event of", parent, err)
 		return
 	}
-	log.Println("trying trail file", filename)
+	log.Println("[info] Trying trail file", filename)
 	f := newTrailFile(filename, tag, SEEK_TAIL)
 	defer f.Close()
 
@@ -35,7 +47,7 @@ EVENT:
 				continue EVENT // ignore
 			}
 			if ev.IsDelete() || ev.IsRename() {
-				log.Println(ev)
+				log.Println("[info]", ev)
 				f.tailAndSend(ch)
 				f.Close()
 				f = newTrailFile(filename, tag, SEEK_HEAD)
