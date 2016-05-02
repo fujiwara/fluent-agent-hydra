c69161216d056e2030e391756bfc40e40974a7bd
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2016-05-02 16:31:03 +0900

    fix connection issue when resolved address is a ipv6.

--- fluent/fluent.go
+++ fluent/fluent.go
@@ -27,6 +27,7 @@ import (
 	"math"
 	"math/rand"
 	"net"
+	"strings"
 	"sync"
 	"time"
 )
@@ -139,7 +140,16 @@ func (f *Fluent) connect() (err error) {
 	}
 	// for DNS round robin
 	n := Rand.Intn(len(addrs))
-	resolved := fmt.Sprintf("%s:%s", addrs[n], port)
+	addr := addrs[n]
+	var format string
+	if strings.Contains(addr, ":") {
+		// v6
+		format = "[%s]:%s"
+	} else {
+		// v4
+		format = "%s:%s"
+	}
+	resolved := fmt.Sprintf(format, addr, port)
 	log.Printf("[info] Connect to %s (%s)", f.Server, resolved)
 	f.conn, err = net.DialTimeout("tcp", resolved, f.Config.Timeout)
 	f.recordError(err)
---
+++ fluent/fluent_test.go
@@ -0,0 +1,56 @@
+package fluent_test
+
+import (
+	"log"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/fujiwara/fluent-agent-hydra/fluent"
+)
+
+func TestConnectToServer(t *testing.T) {
+	hosts := []string{
+		"127.0.0.1",
+		"localhost",
+		"[::1]",
+	}
+	for _, host := range hosts {
+		port := startDummyServer(host)
+		conf := fluent.Config{
+			Server:  host + ":" + port,
+			Timeout: time.Second * 1,
+		}
+		f, err := fluent.New(conf)
+		if err != nil {
+			t.Error(err)
+		}
+		if !f.Alive() {
+			t.Error("server is not available")
+		}
+		f.Close()
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func startDummyServer(host string) string {
+	ch := make(chan string)
+	go func() {
+		l, err := net.Listen("tcp", host+":0")
+		if err != nil {
+			panic(err)
+		}
+		defer l.Close()
+		addr := l.Addr().String()
+		_, port, err := net.SplitHostPort(addr)
+		if err != nil {
+			panic(err)
+		}
+		ch <- port
+		for {
+			conn, _ := l.Accept()
+			log.Println("acccept", conn.LocalAddr())
+		}
+	}()
+	return <-ch
+}
